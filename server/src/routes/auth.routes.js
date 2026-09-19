7ed26b89ce5d852517a9dca215a87b849e86f992
Geetansh Patle <geetanshpatle2@gmail.com>
2026-09-19 10:42:38 +0530

    delete

--- server/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/auth.controller');
-
-router.post('/register', ctrl.register);
-router.post('/login', ctrl.login);
-router.post('/refresh', ctrl.refresh);
-
-module.exports = router;
